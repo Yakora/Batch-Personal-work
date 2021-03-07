76ae4d219589c34ae573213472fabf16ed057d71
Federico <67800864+Yakora@users.noreply.github.com>
2021-03-07 17:13:17 +0100

    Add files via upload

---
+++ martix LMFAO.bat
@@ -0,0 +1,6 @@
+@echo off
+color 02
+:SCROLLR
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+
+goto SCROLLR
